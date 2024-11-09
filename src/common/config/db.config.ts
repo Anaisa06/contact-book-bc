10893ece543aa2ede70911d626e3e83c40be80ed
Anaisa06 <isa.alzate98@gmail.com>
2024-11-08 21:16:17 -0500

    feat: change db config

--- src/common/config/db.config.ts
+++ src/common/config/db.config.ts
@@ -11,12 +11,12 @@ export default class DatabaseConfig implements TypeOrmOptionsFactory {
     createTypeOrmOptions(): TypeOrmModuleOptions {
         return {
             type: 'postgres',
-            // host: this.configService.get<string>('DB_HOST'),
-            // port: this.configService.get<number>('DB_PORT'),
-            // username: this.configService.get<string>('DB_USER'),
-            // password: this.configService.get<string>('DB_PASSWORD'),
-            // database: this.configService.get<string>('DB_NAME'),
-            url: this.configService.get<string>('DB_URL'),
+            host: this.configService.get<string>('DB_HOST'),
+            port: this.configService.get<number>('DB_PORT'),
+            username: this.configService.get<string>('DB_USER'),
+            password: this.configService.get<string>('DB_PASSWORD'),
+            database: this.configService.get<string>('DB_NAME'),
+            // url: this.configService.get<string>('DB_URL'),
             autoLoadEntities: true,
             entities: [],
             synchronize: true,
